4cf40b2bc2b838b10519b1a619581b72f9b30dcf
Dev Raj Raj <>
2021-07-30 21:00:07 +0000

    path to functions.js

--- commands/rps.js
+++ commands/rps.js
@@ -1,5 +1,5 @@
 const { RichEmbed } = require("discord.js");
-const { promptMessage } = require("functions.js");
+const { promptMessage } = require("./functions.js");
 const chooseArr = ["🗻", "📰", "✂"];
 
 module.exports = {
