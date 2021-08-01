94f231ace029d1bf87a2809fe47925c5162545a0
ValCordDev <developingcuber@gmail.com>
2021-08-01 20:19:30 +0200

    Whois command updated

--- commands/whois.js
+++ commands/whois.js
@@ -1,11 +1,12 @@
 const { MessageEmbed } = require('discord.js');
-const { getMember } = require("../functions.js");
 
 module.exports = {
   name:'whois',
-  execute(message, args){
+  execute: async (message, args) => {
     const user = message.mentions.users.first() || message.author;
-    const member = getMember(message, user.username);
+
+    const member = await message.guild.members.fetch({ user, force: true })
+
     const embed = new MessageEmbed()
     .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: true }))
     .setColor(0xF5F5DC)
