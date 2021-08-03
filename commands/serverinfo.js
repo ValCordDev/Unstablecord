20ccff6764e2d0579e652efb9a072892b1b1d27e
ValCordDev <developingcuber@gmail.com>
2021-08-03 20:37:16 +0200

    same with this

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -8,12 +8,12 @@ module.exports = {
       .setColor(0xF5F5DC)
       .setTitle(`**__Server Information__**`)
       .setAuthor(`${message.guild.name}`, message.guild.iconURL)
-      .addField("**Role Amount:**", `${message.guild.roles.cache.size}`, true)
-      .addField("**Server Owner:**", `${message.guild.owner}`, true)
-      .addField("**Member Count:**", `${message.guild.memberCount}`, true)
+      .addField("**Role Amount:**", `${message.guild.roles.cache.size}`, false)
+      .addField("**Server Owner:**", `${message.guild.owner}`, false)
+      .addField("**Member Count:**", `${message.guild.memberCount}`, false)
       .setDescription(`Server Creation Date: ${message.guild.createdAt}`)
-      .addField("**Channel Amount:**", `${message.guild.channels.cache.size}`, true)
-      .addField("**Guild ID:**", `${message.guild.id}`, true);
+      .addField("**Channel Amount:**", `${message.guild.channels.cache.size}`, false)
+      .addField("**Guild ID:**", `${message.guild.id}`, false);
     message.channel.send(embed);
   }
 }
