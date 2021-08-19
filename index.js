4d4a26881cdebb96d734e322861f4bb11214e69a
Dev Raj Raj <88210628+ValCordDev@users.noreply.github.com>
2021-08-19 19:33:26 +0000

    blob

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const mySecret = process.env['DISCORD_TOKEN']
 const express = require('express');
-const app = express();
+const { Client, MessageEmbed, Collection, MessageAttachment } = require("discord.js");
+const fs = require('fs'); 
+const Mongoose = require('mongoose');
+const Levels = require("discord-xp");
+const {Welcomer, Leaver} = require("canvacord");
+
 const port = 3000;
+const prefix = '#';
+const mySecret = process.env['DISCORD_TOKEN']
+
+const app = express();
+const client = new Client();
+client.commands = new Collection();
+Levels.setURL("mongodb://ValCord:pOx81zone@ytbot.g85r2.mongodb.net:27017/?authSource=admin");
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -9,57 +20,46 @@ app.listen(port, () =>
 	console.log(`Example app listening at http://localhost:${port}`)
 );
 // ================= START BOT CODE ===================
-const { Client, MessageEmbed, Collection, MessageAttachment } = require("discord.js");
 
-const Levels = require("discord-xp");
-Levels.setURL("mongodb://...");
-
-const client = new Client();
-
-const prefix = '#';
-
-const fs = require('fs'); 
-
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://ValCord:<pOx81zone>@ytbot.g85r2.mongodb.net/test', {
+/*
+Mongoose.connect('mongodb+srv://ValCord:pOx81zone@ytbot.g85r2.mongodb.net/test', {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false
-}).then(console.log('Connected to Mongo!'))
-
-client.commands = new Collection();
+}).then(console.log('Connected to Mongo!'));
+*/
 
 const commandFiles = fs
 	.readdirSync('./commands/')
 	.filter(file => file.endsWith('.js'));
+
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
 }
 
-const canvacord = require ("canvacord")
-
 client.on("guildMemberAdd", async member => {
   if(member.guild.id !== "754252071519256587") return;
-  const welcomeCard = new canvacord.Welcomer()
-  .setUsername(member.user.username)
-  .setDiscriminator(member.user.discriminator)
-  .setAvatar(member.user.displayAvatarURL({format: "png"}))
-  .setColor("#f5f5dc")
-  .setColor("username-box", "#f5f5dc")
-  .setColor("discriminator-box", "#f5f5dc")
-  .setColor("message-box", "#f5f5dc")
-  .setColor("border", "#f5f5dc")
-  .setColor("avatar", "#f5f5dc")
-  .setBackground("https://cdn.discordapp.com/attachments/871081165208047716/876821477880660008/Welcome_1.png")
-  .setMemberCount(member.guild.memberCount)
-  let attachment = new MessageAttachment(await welcomeCard.build(), "welcome.png")
-  member.guild.channels.cache.get("876828141799239770").send(member.user.toString(), attachment)
-});
-client.on('guildMemberAdd', member => {
-	let welcomeRole = member.guild.roles.cache.find(
-		role => role.name === 'Member'
-	);
+
+  //Welcome card
+  const welcomeCard = new Welcomer()
+    .setUsername(member.user.username)
+    .setDiscriminator(member.user.discriminator)
+    .setAvatar(member.user.displayAvatarURL({format: "png"}))
+    .setColor("#f5f5dc")
+    .setColor("username-box", "#f5f5dc")
+    .setColor("discriminator-box", "#f5f5dc")
+    .setColor("message-box", "#f5f5dc")
+    .setColor("border", "#f5f5dc")
+    .setColor("avatar", "#f5f5dc")
+    .setBackground("https://cdn.discordapp.com/attachments/871081165208047716/876821477880660008/Welcome_1.png")
+    .setMemberCount(member.guild.memberCount);
+  let attachment = new MessageAttachment(await welcomeCard.build(), "welcome.png");
+  member.guild.channels.cache.get("876828141799239770").send(member.user.toString(), attachment);
+  //End welcome card
+
+  //Another welcome card
+  let welcomeRole = member.guild.roles.cache.find(role => role.name === 'Member');
 	member.roles.add(welcomeRole);
 	const embed = new MessageEmbed()
 		.setAuthor(
@@ -85,45 +85,49 @@ client.on('guildMemberAdd', member => {
 		)
 		.setFooter('Enjoy your time here!')
 		.setColor(0xf5f5dc);
-	//member.guild.channels.cache.each(ch => console.log(ch.name))
-	const channel = member.guild.channels.cache.find(
-		ch => ch.name === '👋welcome'
-	);
-	// Do nothing if the channel wasn't found on this server
-	if (!channel) return;
-	// Send the message, mentioning the member
-	channel.send(`Hey ${member}`);
-	channel.send(embed);
+	
+  const channel = member.guild.channels.cache.find(ch => ch.name === '👋welcome');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Hey ${member}`);
+  channel.send(embed);
+  //End another welcome card
 });
+
 client.on("guildMemberRemove", async member => {
   if(member.guild.id !== "754252071519256587") return;
-  const welcomeCard = new canvacord.Leaver()
-  .setUsername(member.user.username)
-  .setDiscriminator(member.user.discriminator)
-  .setAvatar(member.user.displayAvatarURL({format: "png"}))
-  .setColor("#f5f5dc")
-  .setColor("username-box", "#f5f5dc")
-  .setColor("discriminator-box", "#f5f5dc")
-  .setColor("message-box", "#f5f5dc")
-  .setColor("border", "#f5f5dc")
-  .setColor("avatar", "#f5f5dc")
-  .setBackground("https://cdn.discordapp.com/attachments/871081165208047716/876821477880660008/Welcome_1.png")
-  .setMemberCount(member.guild.memberCount)
-  let attachment = new MessageAttachment(await welcomeCard.build(), "bye.png")
-  member.guild.channels.cache.get("866650203330576384").send(member.user.toString(), attachment
-  )});
-client.on('message', message => {
+  const welcomeCard = new Leaver()
+    .setUsername(member.user.username)
+    .setDiscriminator(member.user.discriminator)
+    .setAvatar(member.user.displayAvatarURL({format: "png"}))
+    .setColor("#f5f5dc")
+    .setColor("username-box", "#f5f5dc")
+    .setColor("discriminator-box", "#f5f5dc")
+    .setColor("message-box", "#f5f5dc")
+    .setColor("border", "#f5f5dc")
+    .setColor("avatar", "#f5f5dc")
+    .setBackground("https://cdn.discordapp.com/attachments/871081165208047716/876821477880660008/Welcome_1.png")
+    .setMemberCount(member.guild.memberCount);
+  let attachment = new MessageAttachment(await welcomeCard.build(), "bye.png");
+  member.guild.channels.cache.get("866650203330576384").send(member.user.toString(), attachment);
+});
+
+client.on("message", async (message) => {
+  if (!message.guild) return;
+  if (message.author.bot) return;
+  
+  const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Min 1, Max 30
+  const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
+  if (hasLeveledUp) {
+    const user = await Levels.fetch(message.author.id, message.guild.id);
+    message.channel.send(`${message.author}, congratulations! You have leveled up to **${user.level}**. :tada:`);
+  }
+
 	console.log(`Logged in as ${client.user.tag}!`);
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	client.user.setActivity('#help | linktr.ee/valcord_ for socials :D');
-
-  /*const randomAmountOfXp = Math.floor(Math.random() * 29) + 1;
-   const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
-  if (hasLeveledUp) {
-    const user = await Levels.fetch(message.author.id, message.guild.id);
-    message.channel.send(message.author + "Congratulations! You have leveled up to **" + user.level + "**! :pepe_yes: ")
-  }*/
 
 	const args = message.content.slice(prefix.length).split(/ +/);
 	let command = args
@@ -144,13 +148,11 @@ client.on('message', message => {
   if (command === 'planememe') command = 'avmeme';
   if (command === 'aviationmeme') command = 'avmeme';
   if (command === 'plane') command = 'avmeme';
-
+  if (command === 'lvl') command = 'level';
  
 	if (client.commands.has(command)) {
 		client.commands.get(command).execute(message, args);
-  } /*else {
-		client.commands.get('help').execute(message, []);
-	}
-  */
+  }
 });
+
 client.login(process.env.DISCORD_TOKEN);
